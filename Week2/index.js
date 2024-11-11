71212838c8b9df25c7315fac5881e6b7d3a46110
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-11-12 02:04:11 +0530

    base58 encoding

--- Week2/index.js
+++ Week2/index.js
@@ -47,3 +47,23 @@ console.log(byteArrayFromHex); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
 const uint8Array = new Uint8Array([72, 101, 108, 108, 111]);
 const base64Encoded = Buffer.from(uint8Array).toString("base64");
 console.log(base64Encoded);
+
+// Base58 encode
+const bs58encode = require('bs58').default;
+function uint8ArrayToBase58(uint8Array) {
+  return bs58encode.encode(uint8Array);
+}
+// Example usage:
+const byteArray58 = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+const base58String = uint8ArrayToBase58(byteArray58);
+console.log(base58String); // Output: Base58 encoded string
+
+// Base58 decode
+const bs58decode = require('bs58').default;
+function base58ToUint8Array(base58String) {
+  return bs58decode.decode(base58String);
+}
+// Example usage:
+const base58 = base58String; // Use the previously encoded Base58 string
+const byteArrayFromBase58 = base58ToUint8Array(base58);
+console.log(byteArrayFromBase58); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
