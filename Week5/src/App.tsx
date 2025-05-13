fd9282060c68682dea473a9d2b7d6d28440d0223
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-05-13 12:00:44 +0530

    updated request airdrop

--- Week5/src/App.tsx
+++ Week5/src/App.tsx
@@ -1,17 +1,9 @@
-import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
-import { RequestAirdrop } from './components/RequestAirDrop';
+import { RequestAirdrop } from './components/RequestAirdrop';
 
 function App() {
   return (
@@ -26,6 +18,7 @@ function App() {
                     <div className='mt-8'>
                       <RequestAirdrop />
                     </div>
+
                   </div>
                 </WalletModalProvider>
             </WalletProvider>   
--- Week5/src/components/RequestAirdrop.tsx
+++ Week5/src/components/RequestAirdrop.tsx
@@ -6,19 +6,20 @@ export function RequestAirdrop() {
     const wallet = useWallet();
     const { connection } = useConnection();
 
-    const[amount, setAmount] = useState("");
+    const[amount, setAmount] = useState(0);
 
 
-    function requestAirdrop() {
+    async function requestAirdrop() {
         const publicKey = wallet.publicKey;
-        connection.requestAirdrop(publicKey!, Number(amount) * LAMPORTS_PER_SOL);           // 1 SOL = 10^9 lamports
+        await connection.requestAirdrop(publicKey!, amount * LAMPORTS_PER_SOL);           // 1 SOL = 10^9 lamports
+        alert("Airdropped " + amount + " SOL to " + publicKey!.toBase58());
     }
 
     return <div className="flex gap-6">
         <input onChange={(e) => {
-            setAmount(e.target.value);
+            setAmount(Number(e.target.value));
         }} type="text" placeholder="Amount..." className="text-white border-2 rounded-xl pl-2" />
 
         <button onClick={requestAirdrop} className="border-2 text-white bg-blue-500 rounded-full p-2 cursor-pointer">Request Airdrop</button>
     </div>
-}
+}   
