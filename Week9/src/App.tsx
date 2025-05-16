9b31cdc89e7f689f3baff34e16d903ec8e5a67bf
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-05-16 13:46:04 +0530

    exchange init

---
+++ Week9/src/App.tsx
@@ -0,0 +1,11 @@
+import { Connection } from '@solana/web3.js';
+
+function App() {
+  const connection = new Connection('https://api.devnet-beta.solana.com');
+
+  return <div>
+    hi
+  </div>
+}
+
+export default App
