eac88ce6632b07bb6630944dd63445429c2b5a11
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-07-17 23:25:13 +0530

    display v/s debug macro

---
+++ week29/src/main.rs
@@ -0,0 +1,31 @@
+use std::fmt::{Debug, Display};
+
+#[derive(Debug)]                        // debug macro
+
+struct User {
+    username: String,
+    age: i32,
+}
+
+impl Display for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "This is user struct with age {}", self.age)
+    }
+}
+
+// impl Debug for User {                                                                            // implementing debug with using macro(here using debug trait)
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "This is user struct with age {}, name {}", self.age, self.username)
+//     }
+// }
+
+fn main() {
+    let user1 = User {
+        username: String::from("Sada"),
+        age: 23,
+    };
+
+    println!("name: {}, age: {}", user1.username, user1.age);
+    println!("{}", user1);          // Display
+    println!("{:?}", user1);        // Debug =>(:?)
+}
