9015368d8fe75b0de4d6f18c223bf006c3fb5f03
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-11-20 23:55:26 +0530

    number, bool, ,vector, string, loop, , mut, ownership

--- week26/src/main.rs
+++ week26/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
-    println!("Hello, world!");
+    let ans = sum(1, 2);
+    println!("{}", ans);
+
+    let even = is_even(3);
+    println!("{}", even);
+
+    // string
+    let name = String::from("Sada");
+    println!("My name is: {}", name);
+
+    // vector
+    let vec = vec![1, 2, 3];
+    println!("{:?}", vec);
+
+    // condition
+    let num = 13;
+    let is_even = is_even(num);
+    if is_even {
+        println!("Thee given number {} is even", num);
+    } else {
+        println!("Thee given number {} is odd", num);
+    }
+
+    // loop
+    for i in 0..10 {
+        println!("{}", i);
+    }
+
+    // ownership
+    let name = String::from("Sada");
+    let name2 = name;
+
+    println!("{}", name2);
+}
+
+
+// number
+fn sum(a: u32, b:u32) -> u32 {
+    return a + b;
+}
+
+// boolean
+fn is_even(a:u32) -> bool {
+    return a % 2 == 0;
 }
