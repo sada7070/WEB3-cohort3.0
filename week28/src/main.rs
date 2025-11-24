fb3ee4225816cac26b6d265c6c16ba780507d95c
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-11-24 12:52:59 +0530

    generics over structs

--- week28/src/main.rs
+++ week28/src/main.rs
@@ -25,32 +25,56 @@
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// generics and trait bounds
+// // generics and trait bounds
+
+// fn main() {
+//     println!("{}", sum(1.0, 2.0));
+// //    println!("{}", sum(true, false));               // trait bound telling clearly that Add ops cannot be done on bool
+
+
+//     print_variable(1);
+//     print_variable(2.0);
+//     print_variable("sada");
+
+
+//     println!("{}",biggest_num(1, 3));
+// }
+
+// fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+//     return a + b;
+// }
+
+// fn print_variable<T: std::fmt::Display>(a: T) {
+//     println!("{}", a);
+// }
+
+// fn biggest_num<T: Ord>(x: T, y: T) -> T {
+//     if x > y {
+//         return x;
+//     }
+//     return y;
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// generics over structs
+
+struct Rect<T> {
+    width: T,
+    height: T,
+}
+
+impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
+    fn area(&self) -> T {
+        return self.width * self.height;
+    }
+}
 
 fn main() {
-    println!("{}", sum(1.0, 2.0));
-//    println!("{}", sum(true, false));               // trait bound telling clearly that Add ops cannot be done on bool
+    let r = Rect {
+        width: 1.0,
+        height: 4.0,
+    };
 
-
-    print_variable(1);
-    print_variable(2.0);
-    print_variable("sada");
-
-
-    println!("{}",biggest_num(1, 3));
-}
-
-fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
-    return a + b;
-}
-
-fn print_variable<T: std::fmt::Display>(a: T) {
-    println!("{}", a);
-}
-
-fn biggest_num<T: Ord>(x: T, y: T) -> T {
-    if x > y {
-        return x;
-    }
-    return y;
+    println!("{}", r.area());
 }
