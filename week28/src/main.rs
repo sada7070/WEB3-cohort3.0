91d4a9c03c997d37ddbc644a792115caf01d5360
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-11-24 12:21:10 +0530

    generics and trait bounds

--- week28/src/main.rs
+++ week28/src/main.rs
@@ -1,22 +1,56 @@
-use chrono::{Utc, Local};
-use dotenv::dotenv;
-use std::env;
+// package manager(cargo)
+
+// use chrono::{Utc, Local};
+// use dotenv::dotenv;
+// use std::env;
+
+// fn main() {
+//     let utc = Utc::now();
+//     let local = Local::now();
+
+//     println!("UTC time: {}", utc);
+//     println!("Local time: {}", local);
+
+
+//     // dotenv
+//     dotenv().ok();          // initializing dotenv
+
+//     let db_url_result = env::var("DATABASE_URL");
+
+//     match db_url_result {
+//         Ok(str) => println!("{}", str),
+//         Err(e) => println!("{}", e)
+//     }
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// generics and trait bounds
 
 fn main() {
-    let utc = Utc::now();
-    let local = Local::now();
-
-    println!("UTC time: {}", utc);
-    println!("Local time: {}", local);
+    println!("{}", sum(1.0, 2.0));
+//    println!("{}", sum(true, false));               // trait bound telling clearly that Add ops cannot be done on bool
 
 
-    // dotenv
-    dotenv().ok();          // initializing dotenv
+    print_variable(1);
+    print_variable(2.0);
+    print_variable("sada");
 
-    let db_url_result = env::var("DATABASE_URL");
 
-    match db_url_result {
-        Ok(str) => println!("{}", str),
-        Err(e) => println!("{}", e)
+    println!("{}",biggest_num(1, 3));
+}
+
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    return a + b;
+}
+
+fn print_variable<T: std::fmt::Display>(a: T) {
+    println!("{}", a);
+}
+
+fn biggest_num<T: Ord>(x: T, y: T) -> T {
+    if x > y {
+        return x;
     }
+    return y;
 }
