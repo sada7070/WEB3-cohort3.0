e8b67f6b0ca70582921a5b46a5f3495937fb7694
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-11-25 15:09:55 +0530

    Borsh

--- week30/src/main.rs
+++ week30/src/main.rs
@@ -1,33 +1,70 @@
-// Serde(serialization & decerialization)
+// // Serde(serialization & decerialization)
 
-use serde::{Deserialize, Serialize, de};
+// use serde::{Deserialize, Serialize, de};
 
-#[derive(Debug, Serialize, Deserialize)]
+// #[derive(Debug, Serialize, Deserialize)]
+
+// struct User {
+//     username: String,
+//     password: String,
+// }
+
+// fn main() {
+//     // serializeation
+//     let u = User {
+//         username: String::from("Sada"),
+//         password: String::from("123abc"),
+//     };
+//     let serialized_string = serde_json::to_string(&u);
+
+//     match serialized_string {
+//         Ok(str) => println!("{}", str),
+//         Err(e) => println!("{}", e),
+//     }
+
+//     //decerialization
+//     let s = String::from("{\"username\": \"Sada\", \"password\": \"123fjan\"}");
+//     let decerailized_string: Result<User, serde_json::Error> = serde_json::from_str(&s);
+
+//     match decerailized_string {
+//         Ok(json) => println!("{:?}", json),
+//         Err(e) => println!("{}", e),
+//     }
+// }
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Borsh
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 
 struct User {
     username: String,
-    password: String,
+    password:String,
 }
 
 fn main() {
-    // serializeation
     let u = User {
         username: String::from("Sada"),
-        password: String::from("123abc"),
+        password: String::from("12324"),
     };
-    let serialized_string = serde_json::to_string(&u);
 
-    match serialized_string {
-        Ok(str) => println!("{}", str),
+    let mut v: Vec<u8> = Vec::new();                            // vector to store serialized bytes
+
+    let ans = u.serialize(&mut v);   // serialization
+
+    match ans {
+        Ok(_) => println!("{:?}", v),
         Err(e) => println!("{}", e),
     }
 
-    //decerialization
-    let s = String::from("{\"username\": \"Sada\", \"password\": \"123fjan\"}");
-    let decerailized_string: Result<User, serde_json::Error> = serde_json::from_str(&s);
+    let user = User::try_from_slice(&v);    // deserialization
 
-    match decerailized_string {
-        Ok(json) => println!("{:?}", json),
+    match user {
+        Ok(_) => println!("{:?}", user),
         Err(e) => println!("{}", e),
     }
 }
