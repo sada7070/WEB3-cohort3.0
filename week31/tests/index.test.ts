4bda866e612467684012c7ba99de4dcaa97a748d
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-12-02 23:14:57 +0530

    created system and data account

---
+++ week31/tests/index.test.ts
@@ -0,0 +1,35 @@
+import { expect, test } from "bun:test";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { COUNTER_SIZE } from "./types";
+
+let adminAccount = Keypair.generate();
+let dataAccount = Keypair.generate();
+
+const connection = new Connection("http://127.0.0.1:8899");
+const PROGRAM_ID = new PublicKey("6Mib5LvBb6aWuPqCDEFFKWK5PiKJQe8m3sHRhfW2usn2");
+
+test("Accoubt initiated", async() => {
+    const txn = await connection.requestAirdrop(adminAccount.publicKey, 1 * LAMPORTS_PER_SOL);      // airdroping some SOL to adminAccount
+    await connection.confirmTransaction(txn);
+
+    // creating dataAccount
+    const lamports = await connection.getMinimumBalanceForRentExemption(COUNTER_SIZE);
+
+    const createAccountInstruction = SystemProgram.createAccount({
+        fromPubkey: adminAccount.publicKey,
+        lamports,
+        space: COUNTER_SIZE,
+        programId: PROGRAM_ID,                              // this PROGRAM_ID owns this dataAccount we are creating
+        newAccountPubkey: dataAccount.publicKey,            // assigned the public key we generating at first
+    });
+
+    const createAccountTxn = new Transaction();
+    createAccountTxn.add(createAccountInstruction);         // adding instructions to the transaction
+
+    const signature = await connection.sendTransaction(createAccountTxn, [adminAccount, dataAccount]);
+
+    await connection.confirmTransaction(signature);
+
+    console.log(dataAccount.publicKey.toBase58());
+})
+
