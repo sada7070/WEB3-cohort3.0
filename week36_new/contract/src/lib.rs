430f6b2cf4be8360487535fb68203ae9239851ea
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-12-19 12:49:17 +0530

    double contract

---
+++ week36_new/contract/src/lib.rs
@@ -0,0 +1,37 @@
+// program that doubles the value
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    entrypoint
+};
+
+entrypoint!(process_instruction);
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct OnChainData {
+    count: u32
+}
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    let mut iter = accounts.iter();
+    let data_account = next_account_info(&mut iter)?;
+
+    let mut counter = OnChainData::try_from_slice(&mut data_account.data.borrow_mut())?;
+
+    if counter.count == 0 {
+        counter.count = 1;
+    } else {
+        counter.count = counter.count * 2;
+    }
+
+    counter.serialize(&mut *data_account.data.borrow_mut());
+
+    Ok(())
+}
