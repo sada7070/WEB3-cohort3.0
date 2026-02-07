814f81a60a9eb43547273331439bddaaa835077e
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2026-02-08 01:48:01 +0530

    smart contract

--- week38/calculator/programs/calculator/src/lib.rs
+++ week38/calculator/programs/calculator/src/lib.rs
@@ -6,11 +6,75 @@ declare_id!("4QauPewhHDE1jTNUypZZwqeoMPdTzNtc12QKVxYvgemk");
 pub mod calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init(ctx: Context<Initialize>, init_value: u32) -> Result<()> {
+        ctx.accounts.account.num = init_value;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, num:u32) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num + num;
+        Ok(())
+    }
+
+    pub fn sub(ctx: Context<Sub>, num:u32) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num - num;
+        Ok(())
+    }
+
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num * 2;
+        Ok(())
+    }
+
+    pub fn half(ctx: Context<Half>) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num / 2;
         Ok(())
     }
 }
 
+#[account]
+struct DataShape{
+    pub num: u32
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 4)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Sub<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Half<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
