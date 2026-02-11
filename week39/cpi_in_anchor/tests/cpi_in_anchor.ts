a2c4efd6261e20b0cbfb62268f691190eea3222d
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2026-02-11 17:37:01 +0530

    added test

--- week39/cpi_in_anchor/tests/cpi_in_anchor.ts
+++ week39/cpi_in_anchor/tests/cpi_in_anchor.ts
@@ -1,16 +1,25 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CpiInAnchor } from "../target/types/cpi_in_anchor";
+import { assert } from "chai";
 
 describe("cpi_in_anchor", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+  const recipient = anchor.web3.Keypair.generate();
 
   const program = anchor.workspace.cpiInAnchor as Program<CpiInAnchor>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.solTransfer(new anchor.BN(1000000000)).accounts({
+      sender: provider.publicKey,
+      recepient: recipient.publicKey,
+    }).rpc();
     console.log("Your transaction signature", tx);
+
+    const account = await provider.connection.getAccountInfo(recipient.publicKey);
+    assert.equal(account?.lamports, 1000000000);
   });
 });
